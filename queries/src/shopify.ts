bca174bf34dd5dbfa639a2025a7ea9e8b87e11b6
Mark Morcos <mark.yehia@gmail.com>
2025-05-25 21:02:19 +0200

    chore: pin shopify client

--- queries/src/shopify.ts
+++ queries/src/shopify.ts
@@ -2,6 +2,6 @@ import { createAdminApiClient } from "@shopify/admin-api-client";
 
 export const client = createAdminApiClient({
   storeDomain: process.env.SHOPIFY_STORE!,
-  apiVersion: "2024-07",
+  apiVersion: "2024-01",
   accessToken: process.env.SHOPIFY_KEY!,
 });
