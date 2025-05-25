0eff3b7e4691b3db3658dc031048497e24c63d01
Mark Morcos <mark.yehia@gmail.com>
2025-05-25 18:34:26 +0200

    fix: shopify version

--- queries/src/shopify.ts
+++ queries/src/shopify.ts
@@ -2,6 +2,6 @@ import { createAdminApiClient } from "@shopify/admin-api-client";
 
 export const client = createAdminApiClient({
   storeDomain: process.env.SHOPIFY_STORE!,
-  apiVersion: "2024-01",
+  apiVersion: "2024-07",
   accessToken: process.env.SHOPIFY_KEY!,
 });
